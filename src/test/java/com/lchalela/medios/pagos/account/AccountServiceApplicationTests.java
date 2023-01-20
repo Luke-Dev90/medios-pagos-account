a60aafba3499fa28ea0336711c7b77d5225779c4
lucaschalela990 <lukee_sf@hotmail.com>
2023-01-20 15:44:56 -0300

    init MS account service

---
+++ src/test/java/com/lchalela/medios/pagos/account/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchalela.medios.pagos.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
